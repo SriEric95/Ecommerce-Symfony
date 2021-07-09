9b7d183ba1dbe012fb00409ca251463bba27da19
Srivatsa <pouroucheric@gmail.com>
2021-07-09 15:30:45 +0200

    Modification titre du site

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -30,7 +30,7 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Ecommerce');
+            ->setTitle('HelloShop');
     }
 
     public function configureMenuItems(): iterable
